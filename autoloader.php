b1af8399a82dadb9d3c2a3ee40a698f280c14dbd
Ronald Emanuel <emronald15el@gmail.com>
2021-08-30 10:40:38 +0000

    chore: create autoload

--- autoloader.php
+++ autoloader.php
@@ -5,6 +5,9 @@ spl_autoload_register(function (string $className) {
   $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $filePath);
   $filePath .= '.php';
 
+  echo $filePath;
+  exit();
+
   if (file_exists($filePath)) {
     require_once $filePath;
   }
